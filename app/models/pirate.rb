2e74da21b067a26caf87bc85b4999e0d121766c9
Travis Exline <t.exline22@gmail.com>
2019-07-01 19:28:30 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  @@all = []
+
+  def initialize
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@ships << self
+  end
+
+  def self.all 
+    @@ships
+  end
 end
